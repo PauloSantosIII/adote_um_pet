099fbc92a0582ea046b9583a63f08f5114493509
Paulo Santos III <68072348+PauloSantosIII@users.noreply.github.com>
2022-06-08 17:15:55 -0300

    Terminada aplicação do django

---
+++ bd-pets/adocao/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Adocao
+from pet.serializers import PetSerializer
+from pet.models import Pet
+
+class AdocaoSerializer(serializers.ModelSerializer):
+  pet = PetSerializer(many=False, read_only=True)
+  pet_id = serializers.PrimaryKeyRelatedField(
+    many=False, write_only=True, queryset=Pet.objects.all()
+  )
+
+  class Meta:
+    model = Adocao
+    fields = ('id', 'email', 'valor', 'pet', 'pet_id')
+
+  def create(self, validated_data):
+    validated_data['pet'] = validated_data.pop('pet_id')
+
+    return super().create(validated_data)
+
+  def validate_valor(self, value):
+    if value < 1:
+      return serializers.ValidationError('Valor deve ser maior que 1')
+    if value > 100:
+      return serializers.ValidationError('Valor deve ser menor que 100')
+    return value
